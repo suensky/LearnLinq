29d37556a29885fd16150c38b4f73b620354968a
Huanhuan Sun <husun@microsoft.com>
2017-05-17 14:44:28 -0700

    Add project files.

---
+++ LearnLinq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
